8465e459d71a475c05eaf9c39c5a1453eea493b5
Vikas Mishra <tasu007@gmail.com>
2019-12-27 09:52:28 +0530

    collection iteration

--- Basic_Loops/Basic_Loops/Program.cs
+++ Basic_Loops/Basic_Loops/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Basic_Loops
 {
@@ -10,7 +6,33 @@ namespace Basic_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! here i am Git");//CW is the shorcut for Console.WriteLine
+            //PrintingGitForNthIteration();
+            PrintCollection();
+
+        }
+
+        private static void PrintCollection()
+        {
+            string[] users = {"Jon", "Mary", "Tom", "Reddington", "Anderson"};
+            Console.WriteLine("-----------Using for each---------------");
+            foreach (var item in users)
+            {
+                Console.WriteLine(item);   
+            }
+
+            Console.WriteLine("-------------Using classical for--------------");
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine(users[i]);
+            }
+        }
+
+        /// <summary>
+        /// Iteration based on user input,
+        /// </summary>
+        private static void PrintingGitForNthIteration()
+        {
+            Console.WriteLine("Hello World! here i am Git"); //CW is the shorcut for Console.WriteLine
             Console.WriteLine("Please enter the number of times you want to print 'Git'");
             string strValPrintGit = Console.ReadLine();
             Console.WriteLine("Would you like to print each git in new line or in same line[y/n]");
@@ -21,14 +43,13 @@ namespace Basic_Loops
 
             for (int i = 0; i < nTimesGit; i++)
             {
-                if(decisionToPrint.Key == ConsoleKey.Y)
+                if (decisionToPrint.Key == ConsoleKey.Y)
                     Console.WriteLine("Git");
                 else
                 {
                     Console.Write("Git");
                 }
             }
-
         }
     }
 }
