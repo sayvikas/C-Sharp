6dd07ea1a4add6a8adb2f3c491fe579cff71dc7d
Vikas Mishra <tasu007@gmail.com>
2019-12-27 09:41:04 +0530

    Adding Loop Behaviour

--- Basic_Loops/Basic_Loops/Program.cs
+++ Basic_Loops/Basic_Loops/Program.cs
@@ -10,7 +10,25 @@ namespace Basic_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! here i am Git");
+            Console.WriteLine("Hello World! here i am Git");//CW is the shorcut for Console.WriteLine
+            Console.WriteLine("Please enter the number of times you want to print 'Git'");
+            string strValPrintGit = Console.ReadLine();
+            Console.WriteLine("Would you like to print each git in new line or in same line[y/n]");
+            var decisionToPrint = Console.ReadKey();
+            Console.WriteLine();
+            int nTimesGit = 0;
+            int.TryParse(strValPrintGit, out nTimesGit);
+
+            for (int i = 0; i < nTimesGit; i++)
+            {
+                if(decisionToPrint.Key == ConsoleKey.Y)
+                    Console.WriteLine("Git");
+                else
+                {
+                    Console.Write("Git");
+                }
+            }
+
         }
     }
 }
